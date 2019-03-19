2240c789e822727c2294bdf34b603fa1e56d4e11
Weslley Braga <wesbragagt@gmail.com>
2019-03-19 16:16:37 -0500

    test if users get real time updates

--- game.js
+++ game.js
@@ -26,127 +26,127 @@ var session = {
     activePlayer: 0,
     gamePlaying: true
 };
-init();
-
-// reference data in firebase to populate the page
-database.ref().on("value", function(snapshot) {
-    var session = snapshot.val();
-    console.log(session);
-});
+// init();
 
 document.querySelector(".btn-roll").addEventListener("click", function() {
     if (session.gamePlaying) {
         // Random Number
         session.diceRoll = Math.floor(Math.random() * 6) + 1;
+        // add score
+        session.roundScore += session.diceRoll;
+        // updates database
+        database.ref().set(session);
 
-        // on click display the dice roll number
-        document.querySelector(".dice").src =
-            "assets/images/dice-" + session.diceRoll + ".png";
+        // retrive info from database
+        database.ref().on("value", function(snapshot) {
+            var session = snapshot.val();
+            // display the dice roll number
+            document.querySelector(".dice").src =
+                "assets/images/dice-" + session.diceRoll + ".png";
 
-        document.querySelector(".dice").style.display = "block";
+            document.querySelector(".dice").style.display = "block";
 
-        if (session.diceRoll !== 1) {
-            // add score
-            session.roundScore += session.diceRoll;
+            // display current
             document.querySelector(
                 "#current-" + session.activePlayer
             ).textContent = session.roundScore;
-        } else {
-            alert("You rolled 1, next player");
-            nextPlayer();
-        }
+        });
+
+        // else {
+        //     alert("You rolled 1, next player");
+        //     nextPlayer();
+        // }
 
         // updating info on firebase
-        database.ref().set(session);
     }
 });
 
-document.querySelector(".btn-hold").addEventListener("click", function() {
-    if (session.gamePlaying) {
-        // add current score to global score
-        session.gScore[session.activePlayer] += session.roundScore;
-        //update the UI
-        document.querySelector("#score-" + session.activePlayer).textContent =
-            session.gScore[session.activePlayer];
+// document.querySelector(".btn-hold").addEventListener("click", function() {
+//     if (session.gamePlaying) {
+//         // add current score to global score
+//         session.gScore[session.activePlayer] += session.roundScore;
+//         //update the UI
+//         document.querySelector("#score-" + session.activePlayer).textContent =
+//             session.gScore[session.activePlayer];
 
-        // check if the player won the game
-        if (session.gScore[session.activePlayer] >= 10) {
-            document.querySelector(
-                "#name-" + session.activePlayer
-            ).textContent = "Winner!";
+//         // check if the player won the game
+//         if (session.gScore[session.activePlayer] >= 10) {
+//             document.querySelector(
+//                 "#name-" + session.activePlayer
+//             ).textContent = "Winner!";
 
-            document.querySelector(".dice").style.display = "none";
+//             document.querySelector(".dice").style.display = "none";
 
-            document
-                .querySelector(".player-" + session.activePlayer + "-panel")
-                .classList.add("winner");
-            document
-                .querySelector(".player-" + session.activePlayer + "-panel")
-                .classList.remove("active");
+//             document
+//                 .querySelector(".player-" + session.activePlayer + "-panel")
+//                 .classList.add("winner");
+//             document
+//                 .querySelector(".player-" + session.activePlayer + "-panel")
+//                 .classList.remove("active");
 
-            session.gamePlaying = false;
-        } else {
-            // change player after hold being pressed
-            nextPlayer();
-        }
-    }
-    database.ref().set(session);
-});
+//             session.gamePlaying = false;
+//         } else {
+//             // change player after hold being pressed
+//             nextPlayer();
+//         }
+//     }
+//     database.ref().set(session);
+// });
 
-document.querySelector(".btn-new").addEventListener("click", init);
+// document.querySelector(".btn-new").addEventListener("click", init);
 
-function nextPlayer() {
-    //Next Player's turn reset the roundScore
+// function nextPlayer() {
+//     //Next Player's turn reset the roundScore
 
-    // reset round score to 0
-    session.roundScore = 0;
-    document.querySelector("#current-" + session.activePlayer).textContent =
-        session.roundScore;
+//     // reset round score to 0
+//     session.roundScore = 0;
+//     document.querySelector("#current-" + session.activePlayer).textContent =
+//         session.roundScore;
 
-    //if active player is one then assign 1 to active player, else active player = 0
+//     //if active player is one then assign 1 to active player, else active player = 0
 
-    // ternary operator if active player is 0 then change it to one and vice versa
-    session.activePlayer === 0
-        ? (session.activePlayer = 1)
-        : (session.activePlayer = 0);
+//     // ternary operator if active player is 0 then change it to one and vice versa
+//     session.activePlayer === 0
+//         ? (session.activePlayer = 1)
+//         : (session.activePlayer = 0);
 
-    // change players according to the activePlayer
-    // toggle method checks to see if active class exists if it does it will remove it, if it doesn't it will create it.
-    document.querySelector(".player-0-panel").classList.toggle("active");
-    document.querySelector(".player-1-panel").classList.toggle("active");
+//     // change players according to the activePlayer
+//     // toggle method checks to see if active class exists if it does it will remove it, if it doesn't it will create it.
+//     document.querySelector(".player-0-panel").classList.toggle("active");
+//     document.querySelector(".player-1-panel").classList.toggle("active");
 
-    // then make sure the dice is hidden when the next player starts
-    document.querySelector(".dice").style.display = "none";
-}
+//     // then make sure the dice is hidden when the next player starts
+//     document.querySelector(".dice").style.display = "none";
+// }
 
-function init() {
-    session.diceRoll = 0;
-    session.gScore = [0, 0];
-    session.roundScore = 0;
-    session.activePlayer = 0;
-    session.gamePlaying = true;
+// function init() {
+//     session.diceRoll = 0;
+//     session.gScore = [0, 0];
+//     session.roundScore = 0;
+//     session.activePlayer = 0;
+//     session.gamePlaying = true;
 
-    // when page load do not display the dice
-    document.querySelector(".dice").style.display = "none";
+//     // when page load do not display the dice
+//     document.querySelector(".dice").style.display = "none";
 
-    document.getElementById("score-0").textContent = "0";
-    document.getElementById("score-1").textContent = "0";
+//     document.getElementById("score-0").textContent = "0";
+//     document.getElementById("score-1").textContent = "0";
 
-    document.getElementById("current-0").textContent = "0";
-    document.getElementById("current-1").textContent = "0";
+//     document.getElementById("current-0").textContent = "0";
+//     document.getElementById("current-1").textContent = "0";
 
-    document.getElementById("name-0").textContent = "Player 1";
-    document.getElementById("name-1").textContent = "Player 2";
+//     document.getElementById("name-0").textContent = "Player 1";
+//     document.getElementById("name-1").textContent = "Player 2";
 
-    document.querySelector(".player-0-panel").classList.remove("winner");
-    document.querySelector(".player-1-panel").classList.remove("winner");
+//     document.querySelector(".player-0-panel").classList.remove("winner");
+//     document.querySelector(".player-1-panel").classList.remove("winner");
 
-    document
-        .querySelector(".player-" + session.activePlayer + "-panel")
-        .classList.remove("active");
-    document
-        .querySelector(".player-" + session.activePlayer + "-panel")
-        .classList.add("active");
+//     document
+//         .querySelector(".player-" + session.activePlayer + "-panel")
+//         .classList.remove("active");
+//     document
+//         .querySelector(".player-" + session.activePlayer + "-panel")
+//         .classList.add("active");
 
-    database.ref().set(session);
-}
+//     database.ref().set(session);
+// }
